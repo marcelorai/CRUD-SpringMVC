6748c9b6f1e057bc962411357f18c9b3eb0be9c5
marcelorai <marcelorai1@hotmail.com>
2019-10-30 20:44:56 -0300

    Adicionado as pastas e arquivos do projeto exemplo

---
+++ src/net/codejava/customer/Customer.java
@@ -0,0 +1,54 @@
+package net.codejava.customer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private long id;
+	
+	private String name;
+	private String email;
+	private String address;
+	
+	protected Customer() {		
+	}
+	
+	protected Customer(String name, String email, String address) {
+		this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
